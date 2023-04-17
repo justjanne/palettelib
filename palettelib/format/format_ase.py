41fc90afe298bd928bf3299d034144ce06d344bd
Janne Mareike Koschinski <janne@kuschku.de>
2023-04-17 20:38:54 +0200

    fix: stop merging swatches

--- palettelib/format/format_ase.py
+++ palettelib/format/format_ase.py
@@ -7,55 +7,47 @@ from palettelib.io import PaletteFormat
 from palettelib.palette import Palette, ColorGroup, ColorSwatch
 
 
-def dict_to_swatch(name: str, data: dict[str, list[int]]) -> ColorSwatch:
-    spot = data.get('type', None)
+def dict_to_swatch(entry: dict) -> ColorSwatch:
+    name = entry.get('name', None)
+
+    spot = entry.get('type', None)
     spot = spot == 'Spot'
+
+    data = entry.get('data', {})
+    values = data.get('values', [])
+    mode = data.get('mode')
+
     rgb = None
-    if 'RGB' in data:
-        r, g, b = data.get('RGB')
+    if mode == 'RGB':
+        r, g, b = values
         rgb = ColorRGB(r, g, b)
     cmyk = None
-    if 'CMYK' in data:
-        c, m, y, k = data.get('CMYK')
+    if mode == 'CMYK':
+        c, m, y, k = values
         cmyk = ColorCMYK(c, m, y, k)
     lab = None
-    if 'LAB' in data:
-        l, a, b = data.get('LAB')
+    if mode == 'LAB':
+        l, a, b = values
         lab = ColorLAB(l, a, b)
     gray = None
-    if 'Gray' in data:
-        k, = data.get('Gray')
+    if mode == 'Gray':
+        k, = values
         gray = ColorGrayscale(k)
     return ColorSwatch(name, spot, rgb, cmyk, lab, gray)
 
 
-def dict_list_to_swatch_list(data: list[dict]) -> list[ColorSwatch]:
-    named_swatches: dict[str, dict[str, list[int]]] = {}
-    for swatch in data:
-        name = swatch.get('name', "")
-        entry: Optional[dict] = swatch.get('data', None)
-        if entry is None:
-            continue
-        mode = entry.get('mode', None)
-        if mode is None:
-            continue
-        if name not in named_swatches:
-            named_swatches[name] = {}
-        named_swatches[name][mode] = entry.get('values', [])
-        named_swatches[name]['type'] = entry.get('type', None)
-    return [dict_to_swatch(name, named_swatches[name]) for name in named_swatches]
-
-
 def dict_to_group(data: dict) -> ColorGroup:
     name = data.get('name', "")
-    swatches: list[dict] = data.get('swatches', [])
-    return ColorGroup(name, dict_list_to_swatch_list(swatches))
+    entries: list[dict] = data.get('swatches', [])
+    swatches = [dict_to_swatch(entry) for entry in entries]
+    return ColorGroup(name, swatches)
 
 
 def dict_to_palette(data: list[dict]) -> Palette:
     ungrouped_colors = [entry for entry in data if entry.get('type') in ['Process', 'Spot', 'Global']]
     groups = [dict_to_group(entry) for entry in data if entry.get('type') == 'Color Group']
-    return Palette(name=None, groups=groups, swatches=dict_list_to_swatch_list(ungrouped_colors))
+    ungrouped_swatches = [dict_to_swatch(entry) for entry in ungrouped_colors]
+    return Palette(name=None, groups=groups, swatches=ungrouped_swatches)
 
 
 def swatch_to_dict(swatch: ColorSwatch) -> list[dict]:
